adf918c978ad8d3ae969a19bce80c9f0b6a3800d
GiyoMoon <giyomoon@gmail.com>
2022-05-27 00:27:18 +0200

    :recycle: Update title and prevent Vercel Github comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import type { Component } from 'solid-js'
 const App: Component = () => {
   return (
     <div class='flex justify-center items-center h-full bg-[#fefadc] pointer-events-none select-none'>
-      <img src={ferris} />
+      <img src={ferris} alt='ferris' />
     </div>
   )
 }
