82824b601036911b500dccdae00003132dc5e9ab
GiyoMoon <giyomoon@gmail.com>
2022-05-27 00:40:28 +0200

    :children_crossing: Add link to image author

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,19 @@ import type { Component } from 'solid-js'
 
 const App: Component = () => {
   return (
-    <div class='flex justify-center items-center h-full bg-[#fefadc] pointer-events-none select-none'>
-      <img src={ferris} alt='ferris' />
+    <div class='flex flex-col justify-center items-center h-full bg-[#fefadc] select-none'>
+      <img class='pointer-events-none' src={ferris} alt='ferris' />
+      <p class='mt-10 text-xs'>
+        by{' '}
+        <a
+          class='text-blue-400'
+          href='https://twitter.com/whoisaldeka/status/1165147725542785025'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          @whoisaldeka
+        </a>
+      </p>
     </div>
   )
 }
